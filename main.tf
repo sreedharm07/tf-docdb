460a347e2dc2c9ee1cec4ef52a63da2a14e9555a
sreedhar <sreedhar70755@gmail.com>
2023-08-23 17:14:14 +0530

    tf

--- main.tf
+++ main.tf
@@ -3,3 +3,14 @@ resource "aws_docdb_subnet_group" "main" {
   subnet_ids = var.subnets
   tags       = merge(local.tags, { Name = "${local.name}-subnetgroups" })
 }
+
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "my-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  backup_retention_period = 5
+  preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+}
