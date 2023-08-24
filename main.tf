fd21f89710e5afb6060212a817afcfac8f9a9cc2
sreedhar <sreedhar70755@gmail.com>
2023-08-24 12:38:56 +0530

    tf

--- main.tf
+++ main.tf
@@ -4,3 +4,47 @@ resource "aws_docdb_subnet_group" "main" {
 
   tags = merge(local.tags, {Name="${local.names}-subnet-group"})
 }
+
+
+resource "aws_docdb_cluster" "cluster" {
+  cluster_identifier      = "${local.names}-cluster"
+  engine                  = "docdb"
+  engine_version          = var.engine_version
+  master_username         = var.master_username
+  master_password         = var.master_password
+  backup_retention_period = var.backup_retention_period
+  preferred_backup_window = var.preferred_backup_window
+  skip_final_snapshot     = var.skip_final_snapshot
+  db_subnet_group_name    = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids  = aws_security_group.main.id
+  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.main.name
+}
+
+resource "aws_docdb_cluster_parameter_group" "main" {
+  family      = "docdb4.0"
+  name        = "${local.names}-pg"
+  description = "${local.names}-pg"
+}
+
+resource "aws_security_group" "main" {
+  name        =  "${local.names}-sg"
+  description = "${local.names}-sg"
+  vpc_id      = var.vpc_id
+  tags = merge(local.tags,{Name= "${local.names}-sg"})
+
+  ingress {
+    description      = "docdb"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.sg-ingress-cidr
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
--- var.tf
+++ var.tf
@@ -1,3 +1,13 @@
 variable "env" {}
 variable "tags" {}
 variable "subnet_ids" {}
+variable "engine_version" {}
+variable "master_username" {}
+variable "master_password" {}
+variable "backup_retention_period" {}
+variable "preferred_backup_window" {}
+variable "skip_final_snapshot" {}
+variable "vpc_id" {}
+variable "sg-ingress-cidr" {}
+
+
