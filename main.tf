2080c3b7eea570d32f5bb01fceea19e1e8252244
sreedhar <sreedhar70755@gmail.com>
2023-08-24 14:19:07 +0530

    tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "main" {
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = 2
+  count              = 1
   identifier         = "${local.names}-cluster-instance-${count.index+1}"
   cluster_identifier = aws_docdb_cluster.cluster.id
   instance_class     = var.instance_class
